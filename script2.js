bd9b96287543c3482beaab02c3dcdb0db8ba6ed0
picapauvermelho <141340163+picapauvermelho@users.noreply.github.com>
2025-04-04 07:57:59 -0300

    .çl

--- script2.js
+++ script2.js
@@ -8,13 +8,61 @@ botaoDeAcessibilidade.addEventListener('click', function () {
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
 })
 
-let tamanhoAtualFonte = 1;
+let tamanhoAtualFonte = 1;document.addEventListener('DOMContentLoaded', function(){
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
+    
+    let tamanhoAtualFonte = 1;
+    
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+        const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+        aumentaFonteBotao.addEventListener('click', function(){
+            tamanhoAtualFonte += 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        })
+    
+        diminuiFonteBotao.addEventListener('click', function(){
+            tamanhoAtualFonte -= 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    
+        })
+        })
 
 const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.styldocument.addEventListener('DOMContentLoaded', function(){
+
+            const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+            const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+            
+            botaoDeAcessibilidade.addEventListener('click', function () {
+                botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+                opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+            })
+            
+            let tamanhoAtualFonte = 1;
+            
+            const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+                const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+                aumentaFonteBotao.addEventListener('click', function(){
+                    tamanhoAtualFonte += 0.1;
+                    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+                })
+            
+                diminuiFonteBotao.addEventListener('click', function(){
+                    tamanhoAtualFonte -= 0.1;
+                    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+            
+                })
+                })e.fontSize = `${tamanhoAtualFonte}rem`
     })
 
     diminuiFonteBotao.addEventListener('click', function(){
